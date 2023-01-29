1994311da3efa8fa5d75d940d05afa83bd2d5165
Алена Павлова <alenapavlovaa@mail.ru>
2023-01-29 19:07:32 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+  const [candel, setCandel] = useState(0);
+
+  const {id, image, description, price} = data[candel];
+
+  const [showText, setShowText] = useState(false);
+
+  const nextCandel = () => {
+    setCandel ((candel => {
+      candel ++;
+      if (candel > data.length - 1) {
+        candel = 0;
+      }
+      return candel;
+    }))
+  }
+
+  const previousCandel = () => {
+    setCandel ((candel => {
+      candel --;
+      if (candel < 0) {
+        return data.length - 1;
+      }
+      return candel;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div className='container'>
+     <h1>Выбери свою аромасвечу из соевого воска:</h1>
+     </div>
+
+     <div className='container'>
+      <img src={image} width='300px' alt='candel'/>
+     </div>
+
+     <div className='container'>
+      <p>{id} - {showText ? description : description.substring(0, 48) + '...'}
+      <button className='btn' onClick={() => setShowText(!showText)}>{showText ? 'Показать меньше' : 'Показать больше'}</button>
+      </p>
+     </div>
+
+     <div className='container'>
+      <h2>{price}</h2>
+     </div>
+
+     <div className='btn-container'>
+      <button className='btn-button' onClick={previousCandel}>Previous</button>
+      <button className='btn-button' onClick={nextCandel}>Next</button>
+     </div>
     </div>
   );
 }
